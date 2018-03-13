60704ea9412c29313402879e1e104f7ff9edf223
Luan Nguyen <luan@tklarryonline.me>
2018-03-13 15:34:19 +1100

    [r] 61: Bind the data structure with lesson forms
    https://trello.com/c/gnXWEsTj

--- resort/book/js/booking-lesson-app/lesson/lesson-item.component.js
+++ resort/book/js/booking-lesson-app/lesson/lesson-item.component.js
@@ -7,8 +7,12 @@
             templateUrl: 'js/booking-lesson-app/lesson/lesson-item.html',
             controller: LessonItemController,
             bindings: {
-                date: '<',
-                participants: '<'
+                // variables
+                lesson: '<',
+                participants: '<',
+
+                // functions
+                deleteLesson: '<'
             }
         });
 
@@ -17,24 +21,28 @@
     function LessonItemController(settings) {
         var vm = this;
 
-        vm.ABILITY_LEVELS = settings.ABILITY_LEVELS;
-        vm.results = {
-            date: vm.date,
-            isAM: true,
-            level: vm.ABILITY_LEVELS[0],
-            participants: []
-        };
+        // Initialises
+        this.$onInit = onInit;
 
-        // Binds functions
-        vm.getAmPm = getAmPm;
-        vm.getAmPmCSSClass = getAmPmCSSClass;
+        function onInit() {
+            vm.settings = settings;
+
+            // Binds functions
+            vm.getAmPm = getAmPm;
+            vm.getAmPmCSSClass = getAmPmCSSClass;
+            vm.onDelete = onDelete;
+        }
 
         function getAmPm() {
-            return vm.results.isAM ? 'AM' : 'PM';
+            return vm.lesson.isAM ? 'AM' : 'PM';
         }
 
         function getAmPmCSSClass() {
-            return vm.results.isAM ? 'btn-warning' : 'btn-primary';
+            return vm.lesson.isAM ? 'btn-warning' : 'btn-primary';
+        }
+
+        function onDelete() {
+            vm.deleteLesson(vm.lesson);
         }
     }
 })();
--- resort/book/js/booking-lesson-app/lesson/lessons.controller.js
+++ resort/book/js/booking-lesson-app/lesson/lessons.controller.js
@@ -1,12 +1,3 @@
-function buildDatesRange(fromDate, toDate) {
-    let range = moment.range(fromDate, toDate);
-    let dates = Array.from(range.by('day'));
-
-    return dates.map(function (m) {
-        return m.toDate();
-    });
-}
-
 (function () {
     'use strict';
 
@@ -14,9 +5,14 @@ function buildDatesRange(fromDate, toDate) {
         .module('BookingLessonApp')
         .controller('LessonsController', LessonsController);
 
-    LessonsController.$inject = ['$scope', 'Wix', 'settings'];
+    LessonsController.$inject = [
+        '$scope',
+        'moment',
+        'Wix',
+        'settings'
+    ];
 
-    function LessonsController($scope, Wix, settings) {
+    function LessonsController($scope, moment, Wix, settings) {
         var vm = this;
         var subscription = Wix.subscribe(loadData);
 
@@ -25,20 +21,31 @@ function buildDatesRange(fromDate, toDate) {
         vm.settings = settings;
         vm.lessonType = settings.LESSON_TYPES[0];
         vm.activityType = settings.ACTIVITY_TYPES[0];
+        vm.results = {
+            lessons: []
+        };
 
-        // Binding functions
+        // Binds functions
         vm.getAdultParticipants = getAdultParticipants;
         vm.isDataAvailable = isDataAvailable;
+        vm.addLesson = addLesson;
+        vm.deleteLesson = deleteLesson;
+        vm.updateLessons = updateLessons;
+        vm.getAdultLessons = getAdultLessons;
+        vm.getChildrenLessons = getChildrenLessons;
 
         function loadData(_) {
             $scope.$apply(applyData);
         }
 
         function applyData() {
+            vm.wix = Wix;
             vm.data = Wix.getData();
             vm.dates = buildDatesRange(vm.data.date.checkIn, vm.data.date.checkOut);
             vm.adultParticipants = getAdultParticipants(vm.data.participants);
             vm.childrenParticipants = getChildrenParticipants(vm.data.participants);
+
+            updateLessons();
         }
 
         function isDataAvailable() {
@@ -54,6 +61,83 @@ function buildDatesRange(fromDate, toDate) {
         function getChildrenParticipants(participants) {
             return participants.filter(function (p) {
                 return p.age < 18;
+            });
+        }
+
+        function updateLessons() {
+            // Empties the lessons list when the type is changed
+            vm.results.lessons = [];
+
+            // Then, builds the lessons list based on the chosen type
+            switch (vm.lessonType) {
+                case 'Group':
+                    let lessons = buildLessons('GroupAdult');
+                    let childrenLessons = buildLessons('GroupChildren');
+                    lessons.push.apply(lessons, childrenLessons);
+
+                    vm.results.lessons = lessons;
+                    break;
+                case 'Private':
+                    vm.results.lessons = buildLessons('Private');
+                    break;
+                case 'Private (disabled)':
+                    vm.results.lessons = buildLessons('Private (disabled)');
+                    break;
+            }
+        }
+
+        function addLesson(lesson) {
+            console.log('Add Lesson: ' + lesson);
+            vm.results.lessons.push(lesson);
+        }
+
+        function deleteLesson(lesson) {
+            let idx = vm.results.lessons.indexOf(lesson);
+
+            if (idx < 0) {
+                console.log('Cannot find lesson: ' + lesson);
+                return;
+            }
+
+            console.log('Delete Lesson: ' + lesson);
+            vm.results.lessons.splice(idx, 1);
+        }
+
+        function buildLessons(lessonType) {
+            let lessons = [];
+            for (let i = 0; i < vm.dates.length; i++) {
+                let lesson = {
+                    type: lessonType,
+                    date: vm.dates[i],
+                    isAM: true,
+                    level: settings.ABILITY_LEVELS[0],
+                    participants: []
+                };
+
+                lessons.push(lesson);
+            }
+
+            return lessons;
+        }
+
+        function getAdultLessons() {
+            return vm.results.lessons.filter(function (l) {
+                return l.type === 'GroupAdult';
+            });
+        }
+
+        function getChildrenLessons() {
+            return vm.results.lessons.filter(function (l) {
+                return l.type === 'GroupChildren';
+            });
+        }
+
+        function buildDatesRange(fromDate, toDate) {
+            let range = moment.range(fromDate, toDate);
+            let dates = Array.from(range.by('day'));
+
+            return dates.map(function (m) {
+                return m.toDate();
             });
         }
     }
