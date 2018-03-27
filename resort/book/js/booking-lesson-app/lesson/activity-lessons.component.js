54d6e58b724eb9f700151a4db40dd81ed86265ed
Luan Nguyen <luan@tklarryonline.me>
2018-03-27 12:40:01 +1100

    [r] 158: Bind the activity lessons
    Also:
    - Create new components in _includes folder. Check it out! ;)
    
    https://trello.com/c/a9zvlqwd

--- resort/book/js/booking-lesson-app/lesson/activity-lessons.component.js
+++ resort/book/js/booking-lesson-app/lesson/activity-lessons.component.js
@@ -9,17 +9,20 @@
             bindings: {
                 // variables
                 activity: '<',
+                dates: '<',
+                participants: '<',
 
                 // functions
+                addActivityLessons: '<'
             }
         });
 
     ActivityLessonsController.$inject = ['settings'];
 
     function ActivityLessonsController(settings) {
-        var vm = this;
+        var self = this;
         var results = {
-            activity: vm.activity,
+            activity: self.activity,
             lessons: {
                 group: {
                     adults: [],
@@ -46,9 +49,69 @@
         this.$onInit = onInit;
 
         function onInit() {
-            vm.settings = settings;
+            self.settings = settings;
 
-            vm.results = results;
+            createAllLessons();
+            self.results = results;
+        }
+
+        function createLessons(type) {
+            let lessons = [];
+
+            for (let i = 0; i < self.dates.length; i++) {
+                lessons.push(Lesson(type, self.dates[i], 4, settings));
+            }
+
+            return lessons;
+        }
+
+        function createAllLessons() {
+            // LN Look for a better way to do this
+            results.lessons.group.adults = createLessons('group.adults');
+            results.lessons.group.children = createLessons('group.children');
+            results.lessons.group.mini = createLessons('group.mini');
+            results.lessons.private.lessons = createLessons('private.lessons');
+            results.lessons.disability.lessons = createLessons('disability.lessons');
+        }
+
+        function deleteLesson(type) {
+            let lessonsList = getLessonsList(type);
+
+            return function (lesson) {
+                let idx = lessonsList.indexOf(lesson);
+
+                if (idx < 0) return console.log('Cannot find lesson in ' + type + ': ' + lesson);
+
+                console.log('Delete lesson in ' + type + ': ' + lesson);
+                lessonsList.splice(idx, 1);
+            }
+        }
+
+        function getLessonsList(type) {
+            let keys = type.split('.');
+            return results.lessons[keys[0]][keys[1]];
+        }
+    }
+
+    function Lesson(type, date, duration, settings) {
+
+        return build();
+
+        function build() {
+            let lesson = {
+                date: date,
+                duration: duration,
+                level: settings.ABILITY_LEVELS[0],
+                participants: []
+            };
+
+            lesson.time = settings.TIME_OPTIONS[isGroup() ? 1 : 0];
+
+            return lesson;
+        }
+
+        function isGroup() {
+            return type.indexOf('group') > -1;
         }
     }
 })();
