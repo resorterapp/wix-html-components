b2dba77c0e82410fc826a956e19383e5889c280e
Luan Nguyen <luan@tklarryonline.me>
2018-04-03 13:37:19 +1000

    [f] Add results-panel component

---
+++ resort/book/js/booking-lesson-app/components/results-panel.component.js
@@ -0,0 +1,16 @@
+/**
+ * Created by tklarryonline on Apr 03, 2018.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('BookingLessonApp')
+        .component('resultsPanel', {
+            templateUrl: 'js/booking-lesson-app/components/results-panel.html',
+            bindings: {
+                results: '<'
+            }
+        });
+})();
