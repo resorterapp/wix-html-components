185ce4d7c018bcbec1ec8ee14c5494c82e821d0e
Luan Nguyen <luan@tklarryonline.me>
2018-04-05 15:27:28 +1000

    [f] 190: Move settings to another file
    https://trello.com/c/FtWEdsMh

---
+++ resort/book/js/booking-lesson-app/app.settings.js
@@ -0,0 +1,54 @@
+/**
+ * Created by tklarryonline on Apr 05, 2018.
+ */
+
+(function () {
+  'use strict';
+
+  const SETTINGS = {
+      LESSON_TYPES: [
+        'Group',
+        'Private',
+        'Private (disabled)'
+      ],
+      ACTIVITY_TYPES: [
+        'Ski',
+        'Snowboard',
+        'Telemark',
+        'Snowmobiling',
+        'Snowbiking',
+        'Snowshoeing'
+      ],
+      ABILITY_LEVELS: [
+        'Yellow',
+        'Green (Light)',
+        'Green (Dark)',
+        'Blue (Light)',
+        'Blue (Dark)',
+        'Black (Light)',
+        'Black (Dark)'
+      ],
+      YES_NO_OPTIONS: [
+        {name: 'Yes', value: true},
+        {name: 'No', value: false}
+      ],
+      DURATION_OPTIONS: [
+        {value: 1, name: '1hr'},
+        {value: 2, name: '2hrs'},
+        {value: 3, name: '3hrs'},
+        {value: 4, name: '4hrs'},
+        {value: 5, name: '5hrs'},
+        {value: 6, name: '6hrs'},
+        {value: 7, name: '7hrs'},
+      ],
+      TIME_OPTIONS: [
+        'AM',
+        'PM',
+        'All day'
+      ]
+    };
+
+  angular
+    .module('BookingLessonApp')
+    .constant('settings', SETTINGS);
+})();
