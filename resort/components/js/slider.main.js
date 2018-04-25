b999105e9f6808fd98930a2468606dfa1124887d
Luan Nguyen <luan@tklarryonline.me>
2018-04-26 01:16:19 +1000

    [f] 315: Get min & max value from query string
    https://trello.com/c/quMaRFye

--- resort/components/js/slider.main.js
+++ resort/components/js/slider.main.js
@@ -4,18 +4,42 @@
 (function () {
   'use strict';
 
-  let elem = document.querySelector('input[type="range"]');
-  let target = document.querySelector('.value');
+  const VALUES = [
+    'Yellow',
+    'Green (Light)',
+    'Green (Dark)',
+    'Blue (Light)',
+    'Blue (Dark)',
+    'Black (Light)',
+    'Black (Dark)',
+  ];
+  let rangeSlider;
+  let displayValue;
 
-  elem.oninput = rangeValue;
-  elem.onchange = buttonClick;
+  init();
+
+  function init() {
+    rangeSlider = document.querySelector('input#range1');
+    displayValue = document.querySelector('.value');
+
+    rangeSlider.oninput = rangeValue;
+    rangeSlider.onchange = buttonClick;
+
+    let params = (new URL(document.location)).searchParams;
+    rangeSlider.max = params.get('max');
+    rangeSlider.min = params.get('min');
+  }
 
   function rangeValue () {
-    const newValue = elem.value;
-    target.innerHTML = newValue;
+    displayValue.innerHTML = rangeSlider.value;
   }
 
   function buttonClick() {
-    window.parent.postMessage(elem.value, '*');
+    window.parent.postMessage(VALUES[parseInt(rangeSlider.value) - 1], '*');
+  }
+
+  function receiveMsg(event) {
+    // LN Continue working on this
+    rangeSlider.value = parseInt(event.data) + 1;
   }
 })();
