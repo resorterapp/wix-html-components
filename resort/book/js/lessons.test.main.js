200195380c5f8526b2535a8049ec7455ed1c889f
Luan Nguyen <luan@tklarryonline.me>
2018-04-28 03:12:14 +1000

    [f] 332: Add sample data & text
    https://trello.com/c/sz4TPrly

--- resort/book/js/lessons.test.main.js
+++ resort/book/js/lessons.test.main.js
@@ -20,9 +20,51 @@
         'Snowbiking',
         'Snowshoeing',
       ],
+      chosenActivities: {
+        '954a5abc-cb96-4393-a56b-3e5798e515a2': {
+          skiChosen: true,
+          snowboardChosen: false,
+          telemarkChosen: false,
+          snowbikeChosen: false,
+          snowmobileChosen: false,
+          snowshoeChosen: false,
+        },
+        'e62a136f-a7b2-4358-957c-f2a51e846fef': {
+          skiChosen: true,
+          snowboardChosen: false,
+          telemarkChosen: false,
+          snowbikeChosen: false,
+          snowmobileChosen: false,
+          snowshoeChosen: false,
+        },
+        '81455ad8-2a27-4c4a-a8b2-c140bcbf2409': {
+          skiChosen: false,
+          snowboardChosen: true,
+          telemarkChosen: false,
+          snowbikeChosen: false,
+          snowmobileChosen: false,
+          snowshoeChosen: false,
+        },
+        '746411de-72ba-4121-a69d-f2911b1a03ad': {
+          skiChosen: false,
+          snowboardChosen: false,
+          telemarkChosen: true,
+          snowbikeChosen: false,
+          snowmobileChosen: false,
+          snowshoeChosen: false,
+        },
+        'd3b287ce-53c2-4c39-af1a-9f559db85265': {
+          skiChosen: true,
+          snowboardChosen: false,
+          telemarkChosen: false,
+          snowbikeChosen: false,
+          snowmobileChosen: false,
+          snowshoeChosen: false,
+        },
+      },
       participants: [
         {
-          _id: uuid.v4(),
+          _id: '954a5abc-cb96-4393-a56b-3e5798e515a2',
           firstName: 'John',
           age: 40,
           skiLevel: 'Black (Light)',
@@ -32,47 +74,7 @@
           physicalDetails: null
         },
         {
-          _id: uuid.v4(),
-          firstName: 'William',
-          age: 40,
-          skiLevel: 'Black (Light)',
-          snowboardLevel: 'None',
-          telemarkLevel: 'None',
-          physicalDisability: false,
-          physicalDetails: null
-        },
-        {
-          _id: uuid.v4(),
-          firstName: 'Johnson',
-          age: 40,
-          skiLevel: 'Black (Light)',
-          snowboardLevel: 'None',
-          telemarkLevel: 'None',
-          physicalDisability: false,
-          physicalDetails: null
-        },
-        {
-          _id: uuid.v4(),
-          firstName: 'Smith',
-          age: 40,
-          skiLevel: 'Black (Light)',
-          snowboardLevel: 'None',
-          telemarkLevel: 'None',
-          physicalDisability: false,
-          physicalDetails: null
-        },
-        {
-          _id: uuid.v4(),
-          firstName: 'Jack',
-          age: 40,
-          skiLevel: 'Black (Light)',
-          snowboardLevel: 'None',
-          telemarkLevel: 'None',
-          physicalDisability: false,
-          physicalDetails: null
-        },
-        {
-          _id: uuid.v4(),
+          _id: 'e62a136f-a7b2-4358-957c-f2a51e846fef',
           firstName: 'Jane',
           age: 36,
           skiLevel: 'Blue (Light)',
@@ -82,7 +84,7 @@
           physicalDetails: null
         },
         {
-          _id: uuid.v4(),
+          _id: '81455ad8-2a27-4c4a-a8b2-c140bcbf2409',
           firstName: 'Will',
           age: 7,
           skiLevel: 'None',
@@ -92,37 +94,17 @@
           physicalDetails: null
         },
         {
-          _id: uuid.v4(),
-          firstName: 'Ray',
-          age: 7,
-          skiLevel: 'None',
-          snowboardLevel: 'Green (Light)',
-          telemarkLevel: 'None',
-          physicalDisability: true,
-          physicalDetails: null
-        },
-        {
-          _id: uuid.v4(),
-          firstName: 'Rick',
-          age: 7,
-          skiLevel: 'None',
-          snowboardLevel: 'Green (Light)',
-          telemarkLevel: 'None',
-          physicalDisability: true,
-          physicalDetails: null
-        },
-        {
-          _id: uuid.v4(),
+          _id: '746411de-72ba-4121-a69d-f2911b1a03ad',
           firstName: 'Lam',
           age: 7,
           skiLevel: 'None',
-          snowboardLevel: 'Green (Light)',
-          telemarkLevel: 'None',
+          snowboardLevel: 'None',
+          telemarkLevel: 'Green (Light)',
           physicalDisability: true,
           physicalDetails: null
         },
         {
-          _id: uuid.v4(),
+          _id: 'd3b287ce-53c2-4c39-af1a-9f559db85265',
           firstName: 'Eliza',
           age: 4,
           skiLevel: 'Yellow',
